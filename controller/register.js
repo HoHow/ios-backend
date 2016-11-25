fd2a83713275592e6df58096e8d5a406e12d129b
wuhoho <wumingho@gmail.com>
2016-11-26 00:42:00 +0800

    remove register router

--- controller/register.js
+++ controller/register.js
@@ -1,6 +1,15 @@
-var Users = require('../model/user');
+// var Users = require('../model/user');
 // var token = require('./token');
-
+// var firebase = require("firebase");
+// var rootRef = firebase.database().ref("users");
+// var config = {
+//     apiKey: "AIzaSyAesJTSuxlXeiQwnbyIqX1KzIGx6yeG0Ss",
+//     authDomain: "iosporject.firebaseapp.com",
+//     databaseURL: "https://iosporject.firebaseio.com",
+//     storageBucket: "iosporject.appspot.com"
+//     // messagingSenderId: "783097981047"
+//   };
+// firebase.initializeApp(config);
 
 var controller = {
   add: add
@@ -13,9 +22,14 @@ function add(req, callback){
 function comparePassword(req, callback){
   if(req.password == req.confirm_password){
     //寫入資料庫
-    var user = Users();
-    
+    // var user = Users();
+
+
   }
+}
+
+function writeUserData(email, password){
+  
 }
 
 
--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,7 @@ var express = require('express');
 var Joi = require('joi');
 var bodyParser = require('body-parser');
 var registerCtrl = require('../controller/register');
+// var firebase = require("firebase");
 var router = express();
 
 
@@ -19,17 +20,16 @@ router.post('/', function(req, res, next){
 
   Joi.validate(req.body, schema, function (err, value) { 
     if(err){
-
       res.json({message:err.message});
     }else{
-      // console.log("a");
-      // if(req.body.email != '' && req.body.password != ''){
-      //   registerCtrl.add(req.body,function(err,message){
-      //     res.json(err?400:200,message)
-      //     console.log("router ok");
-      //   });
+      
+      
+        registerCtrl.add(req.body,function(err,message){
+          res.json(err?400:200,message)
+          console.log("router ok");
+        });
         
-      // }
+      
     }
   });
   
